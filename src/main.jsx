308c8bbd7108ef02144c1c58fb5a142e6df92a73
VERCEL-SYS <soluciones@pailabs.cl>
2026-07-14 22:50:17 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import App from './App';
 import NDFVisualization from './views/NDF';
 import BHSVisualization from './views/BHS';
+import RielesVisualization from './views/RIELES';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/" element={<App />} />
         <Route path="/ndf" element={<NDFVisualization />} />
         <Route path="/bhs" element={<BHSVisualization />} />
+        <Route path="/rieles" element={<RielesVisualization />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </HashRouter>
