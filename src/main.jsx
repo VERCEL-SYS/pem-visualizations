20e877a52a0e33d3549d0cab93adf37b92ba8071
VERCEL-SYS <soluciones@pailabs.cl>
2026-08-20 01:04:01 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import App from './App';
 import NDFVisualization from './views/NDF';
 import BHSVisualization from './views/BHS';
 import RielesVisualization from './views/RIELES';
+import PLEAPipeline from './views/plea-pipeline';          // ← NUEVO
+import SpectrumBioeffect from './views/spectrum-bioeffect'; // ← NUEVO
+import EcosystemMap from './views/ecosystem-map';           // ← NUEVO
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -14,6 +17,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/ndf" element={<NDFVisualization />} />
         <Route path="/bhs" element={<BHSVisualization />} />
         <Route path="/rieles" element={<RielesVisualization />} />
+        <Route path="/plea" element={<PLEAPipeline />} />           {/* ← NUEVO */}
+        <Route path="/spectrum" element={<SpectrumBioeffect />} />  {/* ← NUEVO */}
+        <Route path="/ecosystem" element={<EcosystemMap />} />      {/* ← NUEVO */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </HashRouter>
